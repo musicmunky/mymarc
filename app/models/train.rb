7716d732ca1ded4208d697dc6a8f984c4adf17d2
musicmunky <musicmunky@gmail.com>
2015-10-14 15:52:22 -0400

    quick update

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,4 +1,5 @@
 class Train < ActiveRecord::Base
 	has_and_belongs_to_many :train_stops
+	belongs_to :line
 
 end
--- app/models/train_stop.rb
+++ app/models/train_stop.rb
@@ -1,4 +1,5 @@
 class TrainStop < ActiveRecord::Base
 	has_and_belongs_to_many :trains
+	belongs_to :line
 
 end
